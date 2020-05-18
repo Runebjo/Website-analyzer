4a9d19659f0fc29b302c63e0fb9d6b8a68a30a4b
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-05-18 23:01:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [urlInput, setUrlInput] = useState('');
+	const [url, setUrl] = useState(
+		'https://howtocreateapps.com/wp-json/wp/v2/posts'
+	);
+	const [response, setResponse] = useState(null);
+	const [website, setWebsite] = useState('howtocreateapps.com');
+	const [isLoadingHeaders, setIsLoadingHeaders] = useState(false);
+	const [posts, setPosts] = useState([]);
+
+	function submitUrl(e) {
+		e.preventDefault();
+		setWebsite(urlInput);
+		setPosts([]);
+		setUrl(`https://${urlInput}/wp-json/wp/v2/posts`);
+	}
+
+	function decodeHtml(html) {
+		var txt = document.createElement('textarea');
+		txt.innerHTML = html;
+		return txt.value;
+	}
+
+	function countWords(str) {
+		str = str.replace(/(^\s*)|(\s*$)/gi, '');
+		str = str.replace(/[ ]{2,}/gi, ' ');
+		str = str.replace(/\n /, '\n');
+		return str.split(' ').length;
+	}
+
+	useEffect(() => {
+		setIsLoadingHeaders(true);
+		axios
+			.get(url)
+			.then(response => {
+				setResponse(response);
+				console.log('response', response);
+				setIsLoadingHeaders(false);
+				function getPosts(totalPages) {
+					console.log('totalPages', totalPages);
+					const posts = [];
+					for (let page = 1; page <= totalPages; page++) {
+						const post = axios.get(`${url}?per_page=10&page=${page}`);
+						posts.push(post);
+					}
+
+					console.log('posts', posts);
+					axios
+						.all(posts)
+						.then(res => {
+							const mappedPosts = res.map(r => r.data);
+							const postpost = mappedPosts.flat();
+							const processedPost = postpost.map(p => {
+								return {
+									id: p.id,
+									createdDate: p.date.substr(0, 10),
+									title: decodeHtml(p.title.rendered),
+									numberOfWords: countWords(p.content.rendered),
+								};
+							});
+
+							console.log('processedPost', processedPost);
+							//console.log('postpost', countWords(postpost[0].content.rendered));
+							setPosts(processedPost);
+						})
+						.catch(error => {
+							console.log(`Error getting posts: ${error}`);
+						});
+				}
+				getPosts(response.headers['x-wp-totalpages']);
+			})
+			.catch(error => {
+				setIsLoadingHeaders(false);
+			});
+	}, [url]);
+
+	return (
+		<div>
+			<form onSubmit={submitUrl}>
+				<input
+					type='text'
+					name='url'
+					onChange={e => setUrlInput(e.target.value)}
+					style={{ width: '80%' }}
+				/>
+				<button>Search</button>
+			</form>
+			{isLoadingHeaders ? (
+				<h3>Loading...</h3>
+			) : (
+				<>
+					<h3>Website: {website}</h3>
+					<p>
+						Number of posts:{' '}
+						{response && parseInt(response.headers['x-wp-total'])}
+					</p>
+				</>
+			)}
+			{!isLoadingHeaders && posts.length > 0 ? (
+				<ul>
+					{posts.map(post => (
+						<li key={post.id}>
+							{post.createdDate} - {post.title} - {post.numberOfWords}
+						</li>
+					))}
+				</ul>
+			) : (
+				<h5>Loading posts...</h5>
+			)}
+		</div>
+	);
 }
 
 export default App;
